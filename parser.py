e4090f928c4ded921c6b05190e21ab3431cd1758
lislmaka <70829116+lislmaka@users.noreply.github.com>
2021-03-30 12:26:19 +0300

    test parser

--- parser.py
+++ parser.py
@@ -1,13 +1,70 @@
 import random
+import requests
+import re
+import pprint
+import string
 
-r = 0
-l = 0
 
-for i in range(1000):
-    v = random.randint(0, 1)
-    if v == 0:
-        r += 1
-    else:
-        l += 1
+def find_all_forms(source):
+    pattern = '<form.*?id="frma.*?".*?(.*?)</form>'
+    forms = re.findall(pattern, source, re.DOTALL)
 
-print('l = ', l, ', r = ', r)
+    return forms
+
+
+def find_all_fields(forms):
+    pattern = '\stype="(.*?)".+?name="(.*?)".+?value="(.*?)"'
+    selected_fields = ''
+    max_fields = 100
+
+    for form in forms:
+        fields = [{'type': f_type, 'name': f_name, 'value': f_value} for f_type, f_name, f_value in re.findall(pattern, form, re.DOTALL)]
+        #fields = re.findall(pattern, forms, re.DOTALL)
+        if fields and len(fields) <= max_fields:
+            max_fields = len(fields)
+            selected_fields = fields
+
+    return selected_fields
+
+
+def send_fake_request(fields):
+    for field in fields:
+        if field['type'] == 'text' and not field['value']:
+            letters = string.ascii_uppercase
+            field['value'] = ''.join(random.choice(letters) for i in range(10))
+        elif field['type'] == 'tel' and not field['value']:
+            letters = string.digits
+            field['value'] = ''.join(random.choice(letters) for i in range(10))
+        elif not field['value']:
+            letters = string.ascii_uppercase
+            field['value'] = ''.join(random.choice(letters) for i in range(10))
+
+    for field in fields:
+        print('type = ', field['type'], ', name = ', field['name'], ', value = ', field['value'])
+
+
+def main():
+    url = 'https://lite5.ru/'
+    #url = 'https://abcgarant.ru/'
+    headers = {
+        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36'}
+
+    r = requests.get(url, headers)
+
+    if r.status_code == 200:
+        print('Status code = ', r.status_code)
+
+        forms = find_all_forms(r.text)
+        if forms:
+            print('Total forms = ', len(forms))
+            fields = find_all_fields(forms)
+            if fields:
+                send_fake_request(fields)
+            else:
+                print('No fields were found')
+        else:
+            print('Forms not found on this page - ', url)
+
+
+# Start main prg
+main()
